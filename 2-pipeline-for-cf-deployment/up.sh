031051dd5ed738009f57518b16b483f6fe57a25b
Micah Young <myoung@pivotal.io>
2017-05-02 01:35:30 -0400

    Add bosh to deployment
    - Disable TLS termination

--- 2-pipeline-for-cf-deployment/up.sh
+++ 2-pipeline-for-cf-deployment/up.sh
@@ -9,14 +9,18 @@ if ! [ -d state/ ]; then
 fi
 
 source state/env.sh
-CONCOURSE_TARGET=${CONCOURSE_TARGET:?"env!"}
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:?"env!"}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:?"env!"}
+AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION:?"env!"}
+CONCOURSE_DOMAIN=${CONCOURSE_DOMAIN:?"env!"}
+CONCOURSE_USERNAME=${CONCOURSE_USERNAME:?"env!"}
 CONCOURSE_PASSWORD=${CONCOURSE_PASSWORD:?"env!"}
+CONCOURSE_BOSH_ENV=${CONCOURSE_BOSH_ENV:?"env!"}
+DOMAIN=${DOMAIN:?"env!"}
+CONCOURSE_TARGET=${CONCOURSE_TARGET:?"env!"}
 CONCOURSE_PIPELINE=${CONCOURSE_PIPELINE:?"env!"}
 STATE_REPO_URL=${STATE_REPO_URL:?"env!"}
 STATE_REPO_PRIVATE_KEY=${STATE_REPO_PRIVATE_KEY:?"env!"}
-
-bbl_cmd="bbl --state-dir state/"
-CONCOURSE_DOMAIN=$($bbl_cmd lbs | sed 's/.*\[\(.*\)\]/\1/')  # Format: Concourse LB: stack-bbl-Concours-1RABRZ7DBDC7F [stack-bbl-Concours-1RABRZ7DBDC7F-585187859.us-west-2.elb.amazonaws.com]
 
 mkdir -p bin
 PATH=$(pwd)/bin:$PATH
@@ -31,6 +35,24 @@ if ! [ -f bin/fly ]; then
   chmod +x bin/fly
 fi
 
+if ! [ -f state/rsakey.pem ]; then
+  openssl req \
+    -newkey rsa:2048 \
+    -nodes \
+    -keyout state/$DOMAIN.key \
+    -x509 \
+    -days 365 \
+    -subj "/C=US/ST=NY/O=Pivotal/localityName=NYC/commonName=$DOMAIN/organizationalUnitName=Foo/emailAddress=bar" \
+    -multivalue-rdn \
+    -out state/$DOMAIN.crt \
+  ;
+
+  openssl rsa \
+    -in state/$DOMAIN.key \
+    -out state/rsakey.pem \
+  ;
+fi
+
 if fly login \
   --target $CONCOURSE_TARGET \
   --concourse-url "http://$CONCOURSE_DOMAIN" \
@@ -39,7 +61,7 @@ if fly login \
   fly set-team \
     --target $CONCOURSE_TARGET \
     --team-name main \
-    --basic-auth-username=admin \
+    --basic-auth-username=$CONCOURSE_USERNAME \
     --basic-auth-password=$CONCOURSE_PASSWORD \
     --non-interactive \
   ;
@@ -59,9 +81,15 @@ if ! fly pipelines -t $CONCOURSE_TARGET | grep $CONCOURSE_PIPELINE; then
     --target $CONCOURSE_TARGET \
     --pipeline $CONCOURSE_PIPELINE \
     --config cf-deployment-pipeline.yml \
+    --var bbl_env_name="$CONCOURSE_BOSH_ENV" \
+    --var bbl_aws_region="$AWS_DEFAULT_REGION" \
+    --var bbl_aws_access_key_id="$AWS_ACCESS_KEY_ID" \
+    --var bbl_aws_secret_access_key="$AWS_SECRET_ACCESS_KEY" \
+    --var bbl_lbs_ssl_cert="$(cat state/$DOMAIN.crt)" \
+    --var bbl_lbs_ssl_signing_key="$(cat state/$DOMAIN.key)" \
     --var state_repo_url="$STATE_REPO_URL" \
     --var state_repo_private_key="$STATE_REPO_PRIVATE_KEY" \
-    --var system_domain="cf.$DOMAIN" \
+    --var system_domain="$DOMAIN" \
     --non-interactive \
   ;
 fi
