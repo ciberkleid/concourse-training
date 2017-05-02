f744aa4b1693601b16fb36973cc29b826ad9ee9e
Micah Young <myoung@pivotal.io>
2017-05-02 15:04:22 -0400

    Move certs into env instead of state
    This will mean that commiting to the state wont be needed after up except for env.sh changes

--- 2-pipeline-for-cf-deployment/up.sh
+++ 2-pipeline-for-cf-deployment/up.sh
@@ -19,6 +19,8 @@ CONCOURSE_BOSH_ENV=${CONCOURSE_BOSH_ENV:?"env!"}
 DOMAIN=${DOMAIN:?"env!"}
 CONCOURSE_TARGET=${CONCOURSE_TARGET:?"env!"}
 CONCOURSE_PIPELINE=${CONCOURSE_PIPELINE:?"env!"}
+BBL_LBS_CERT=${BBL_LBS_CERT:?"env!"}
+BBL_LBS_KEY=${BBL_LBS_KEY:?"env!"}
 STATE_REPO_URL=${STATE_REPO_URL:?"env!"}
 STATE_REPO_PRIVATE_KEY=${STATE_REPO_PRIVATE_KEY:?"env!"}
 
@@ -33,24 +35,6 @@ fi
 if ! [ -f bin/fly ]; then
   curl -L "http://$CONCOURSE_DOMAIN/api/v1/cli?arch=amd64&platform=darwin" > bin/fly
   chmod +x bin/fly
-fi
-
-if ! [ -f state/rsakey.pem ]; then
-  openssl req \
-    -newkey rsa:2048 \
-    -nodes \
-    -keyout state/$DOMAIN.key \
-    -x509 \
-    -days 365 \
-    -subj "/C=US/ST=NY/O=Pivotal/localityName=NYC/commonName=$DOMAIN/organizationalUnitName=Foo/emailAddress=bar" \
-    -multivalue-rdn \
-    -out state/$DOMAIN.crt \
-  ;
-
-  openssl rsa \
-    -in state/$DOMAIN.key \
-    -out state/rsakey.pem \
-  ;
 fi
 
 if fly login \
@@ -85,8 +69,8 @@ if ! fly pipelines -t $CONCOURSE_TARGET | grep $CONCOURSE_PIPELINE; then
     --var bbl_aws_region="$AWS_DEFAULT_REGION" \
     --var bbl_aws_access_key_id="$AWS_ACCESS_KEY_ID" \
     --var bbl_aws_secret_access_key="$AWS_SECRET_ACCESS_KEY" \
-    --var bbl_lbs_ssl_cert="$(cat state/$DOMAIN.crt)" \
-    --var bbl_lbs_ssl_signing_key="$(cat state/$DOMAIN.key)" \
+    --var bbl_lbs_ssl_cert="$BBL_LBS_CERT" \
+    --var bbl_lbs_ssl_signing_key="$BBL_LBS_KEY" \
     --var state_repo_url="$STATE_REPO_URL" \
     --var state_repo_private_key="$STATE_REPO_PRIVATE_KEY" \
     --var system_domain="$DOMAIN" \
